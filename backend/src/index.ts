40415216c229fbf01ea9f1fe696d9951c0eef3cd
Nehuen Olivera <nehuenolivera@gmail.com>
2025-11-06 19:29:58 +1000

    Express Server working

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+
+dotenv.config();
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Barber Appointments API is running 🚀");
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
